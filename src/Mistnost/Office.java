ba433ded3036635e97ac00000ffcd5919c81ed30
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-01-19 22:47:16 +0100

    Office

---
+++ src/Mistnost/Office.java
@@ -0,0 +1,13 @@
+package Mistnost;
+
+import java.io.File;
+
+public class Office {
+    private String documents[];
+     File yindate;
+
+     public void searcheDocuments(String documents){
+
+     }
+
+}
