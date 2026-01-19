10698db93004ae1835390e86c2b82aee6d1a1df7
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-01-19 23:09:05 +0100

    Office

--- src/Mistnost/Office.java
+++ src/Mistnost/Office.java
@@ -1,9 +1,11 @@
 package Mistnost;
 
+import GameConcole.GameConsole;
+
 import java.io.File;
 
-public class Office {
-    private String documents[];
+public class Office extends GameConsole {
+     private String documents[];
      File yindate;
 
      public void searcheDocuments(String documents){
