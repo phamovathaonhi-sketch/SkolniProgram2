adff9bd8a4b5e0e36b7217334dd3f2369a3cfb13
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-01-19 22:52:08 +0100

    Laboratory

---
+++ src/Mistnost/Laboratory.java
@@ -0,0 +1,12 @@
+package Mistnost;
+
+import java.io.File;
+
+public class Laboratory {
+    private String dateList[];
+    private int blood;
+
+    public void comparedate(String dateList, File yindate){
+
+    }
+}
