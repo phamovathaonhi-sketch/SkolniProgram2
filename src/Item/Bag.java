c0fc379adb9a54c5b1000273e1d69eb8f0546aa4
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-01-20 20:30:28 +0100

    Bag

---
+++ src/Item/Bag.java
@@ -0,0 +1,20 @@
+package Item;
+
+
+import java.util.ArrayList;
+
+public class Bag extends Item{
+    private ArrayList<Item> items;
+    private boolean full;
+
+    public void addItem(Item i1){
+
+    }
+
+    public void removeItem(Item i2){
+
+    }
+    public void useItem(Item i3){
+
+    }
+}
