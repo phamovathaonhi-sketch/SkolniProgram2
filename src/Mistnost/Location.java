f403871e5e361e99c1fc4f4cce6751029255f5e5
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-01-28 21:59:11 +0100

    Characters

---
+++ src/Mistnost/Location.java
@@ -0,0 +1,21 @@
+package Mistnost;
+
+import Item.Item;
+
+import java.util.List;
+import java.util.Map;
+import Character.NPC;
+import Character.Enemy;
+
+public class Location {
+    private String name;
+    private String theory;
+    private List<Item> items;
+    private Map<String, String> itemPlacement;
+    private NPC npc;
+    private Enemy enemy;
+
+
+
+
+}
