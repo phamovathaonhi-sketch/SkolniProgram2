fcb846562d336531334d026584906c922a8421a4
pham <phamovathaonhi@gmail.com>
2026-02-03 07:47:19 +0100

    l

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import locations.WorldLoader;
 import characters.Player;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Player player = new Player("Detective");
 
         WorldLoader w = new WorldLoader(player);
