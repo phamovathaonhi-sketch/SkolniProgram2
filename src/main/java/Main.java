feef45f3d952bc6bf057f47ff93c8b90364281c7
Thao Nhi Pham <phamovathaonhi@gmail.com>
2026-02-10 20:12:14 +0100

    cviceni

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-package app;
+
 
 import characters.Player;
 import locations.WorldLoader;
--- src/main/java/characters/Character.java
+++ src/main/java/characters/Character.java
@@ -14,17 +14,24 @@ public abstract class Character {
         this.damage = 5;
     }
 
-    public String getName() { return name; }
-    public int getHP() { return HP; }
-    public int getDamage() { return damage; }
+    public String getName() {
+        return name; }
+    public int getHP() {
+        return HP; }
+    public int getDamage() {
+        return damage; }
 
     public void receiveDamage(int dmg) {
         HP -= Math.max(0, dmg);
         if (HP < 0) HP = 0;
     }
 
-    public boolean isDefeated() { return HP <= 0; }
+    public boolean isDefeated() {
+        return HP <= 0;
+    }
 
-    public Location getCurrentLocation() { return currentLocation; }
-    public void setCurrentLocation(Location currentLocation) { this.currentLocation = currentLocation; }
+    public Location getCurrentLocation() {
+        return currentLocation; }
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation; }
 }
